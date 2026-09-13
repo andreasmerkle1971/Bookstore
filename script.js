d7ec071359e4d4400502606149f3fd037d76e73c
andreasmerkle1971 <a.merkle@evoinfo.de>
2026-09-13 16:33:28 +0200

    unshift and link array fixed

--- script.js
+++ script.js
@@ -1,38 +1,34 @@
 function renderBooks() {
     const booksContainer = document.getElementById("book_list");
     if (!booksContainer) return;
-
     booksContainer.innerHTML = "";
-
     for (let index = 0; index < books.length; index++) {
         booksContainer.innerHTML += getNoteTemplate(index);
     }
 }
 function getNoteTemplate(index) {
     const book = books[index];
-
-    const comments = Array.isArray(book.comments) && book.comments.length > 0
-   
-    ? book.comments.join('<br>') 
-    : book.comments;
-
-     console.log(Array.isArray(comments));
-
+    const comments =
+        Array.isArray(book.comments) && book.comments.length > 0
+            ? book.comments.map(c => c.comment).join("<br>")
+            : book.comments;
+    console.log(Array.isArray(comments));
     const formatierterPrice = formatPrice(book.price);
     return `
     <div class="card">
         <div class="books-gallery"><h2>${book.name}</h2></div>
         <div class="books-gallery">Author: ${book.author}</div>
         <div class="books-gallery"><img src="./assets/img/${book.image}" alt="${book.name}" loading = "lazy"></div>
-<div>
-    <div class="like"> <span id="like-count-${index}">likes: ${book.likes} </span> 
-    <img id="like-icon-${index}" src="${book.liked ? "./assets/icons/like.svg" : "./assets/icons/dislike.svg"}" alt="like Bild" 
-    onclick="likeDislikeToggle(${index})"></div>
-</div>
-    <div id="price" class="books-gallery">Preis: ${formatierterPrice}</div>
-    <div class="books-gallery">Veröffentlichungsjahr: ${book.publishedYear}</div>
-    <div class="books-gallery">Genre: ${book.genre}</div>
-    <div class="comments" id="comments-box-${index}">Kommentare: ${comments}</div>
+    <div>
+        <div class="like"> <span id="like-count-${index}">likes: ${book.likes} </span> 
+        <img id="like-icon-${index}" src="${book.liked ? "./assets/icons/like.svg" : "./assets/icons/dislike.svg"}" alt="like Bild" 
+        onclick="likeDislikeToggle(${index})"></div>
+    </div>
+        <div id="price" class="books-gallery">Preis: ${formatierterPrice}</div>
+        <div class="books-gallery">Veröffentlichungsjahr: ${book.publishedYear}</div>
+        <div class="books-gallery">Genre: ${book.genre}</div>
+        <p>Kommentare: </p>
+        <div class="comments" id="comments-box-${index}">${comments}</div>
 <div class="input-box">
         <input class="input-comment" id="input-comment-${index}" type="text" placeholder = " Schreibe Deinen Kommentar" onkeydown="addComment(event, ${index}, 'keydown')">
         <button type="button" onclick="addComment(event, ${index}, 'click')">return</button>
@@ -51,13 +47,10 @@ function formatPrice(price) {
 
 function likeDislikeToggle(index) {
     const book = books[index];
-
     const likeIcon = document.getElementById(`like-icon-${index}`);
     const currentLikes = document.getElementById(`like-count-${index}`);
-
     console.log(likeIcon);
     console.log(currentLikes);
-
     if (book.liked) {
         book.liked = false;
         book.likes--;
@@ -71,25 +64,30 @@ function likeDislikeToggle(index) {
 }
 
 function addComment(event, index, triggerType) {
-    if (triggerType === 'click' || (triggerType === 'keydown' && event.key === "Enter")) {
+    if (
+        triggerType === "click" ||
+        (triggerType === "keydown" && event.key === "Enter")
+    ) {
         const inputField = document.getElementById(`input-comment-${index}`);
         const commentText = inputField.value.trim();
-
+        // const commentReverse = comments.reverse();
         if (commentText !== "") {
             if (!Array.isArray(books[index].comments)) {
                 books[index].comments = [];
             }
+            books[index].comments.unshift({
+          name:"",
+            comment: commentText
+        });
 
-            books[index].comments.push(commentText);
-
-            const commentsBox = document.getElementById(`comments-box-${index}`);
+            const commentsBox = document.getElementById(
+                `comments-box-${index}`,
+            );
             if (commentsBox) {
 
-                commentsBox.innerHTML = `Kommentare:<br>${books[index].comments.join('<br>')} `;
+                commentsBox.innerHTML = `${books[index].comments.map(c=> c.comment).join("<br>")} `;
             }
-          inputField.value = "";
-          
-            
+            inputField.value = "";
         }
     }
 }
