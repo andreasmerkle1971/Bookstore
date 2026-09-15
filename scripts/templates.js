35c3efc9a37afaae6c8d552ebf79a077c479cf72
andreasmerkle1971 <a.merkle@evoinfo.de>
2026-09-15 13:49:52 +0200

    changed "return" in "senden" button

--- scripts/templates.js
+++ scripts/templates.js
@@ -21,7 +21,7 @@ function getNoteTemplate(index) {
         <p>Kommentare: </p><div class="comments"  id="comments-box-${index}">${comments}</div>        
 <div class="input-box">
         <input class="input-comment" id="input-comment-${index}" type="text" placeholder = " Schreibe Deinen Kommentar" onkeydown="addComment(event, ${index}, 'keydown')">
-        <button type="button" onclick="addComment(event, ${index}, 'click')">return</button>
+        <button type="button" onclick="addComment(event, ${index}, 'click')">senden</button>
 </div>
     </div>
     `;
